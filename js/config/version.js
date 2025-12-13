2db6bef4fe92f6788264c613847ae741d8fa8e9e
tstrembu <81712317+tstrembu@users.noreply.github.com>
2025-12-12 22:37:11 -0500

    Update version.js

--- js/config/version.js
+++ js/config/version.js
@@ -4,7 +4,7 @@
 // ======================================
 
 export const VMQ_VERSION = '3.0.5';
-export const VMQ_BUILD_DATE = '2025-12-07';
+export const VMQ_BUILD_DATE = '2025-12-12';
 export const VMQ_RELEASE = 'Bieler Method + ML Adaptive + 8 Engines LIVE';
 export const VMQ_REPO_URL = 'https://github.com/violin-mastery-quest/vmq';
 
