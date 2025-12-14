27b0d929c4087f337d20b1eeb69e0a2b2537c919
tstrembu <81712317+tstrembu@users.noreply.github.com>
2025-12-14 17:45:14 -0500

    Create bootstrap.js

---
+++ js/bootstrap.js
@@ -0,0 +1,10 @@
+// js/bootstrap.js
+// Placeholder module to satisfy Service Worker precache list.
+// This file is intentionally minimal.
+//
+// If you later split shell-level boot logic out of App.js / index.html,
+// you can move shared startup utilities here.
+
+export const BOOTSTRAP_VERSION = 'vmq-bootstrap-1';
+
+export function noop() {}
